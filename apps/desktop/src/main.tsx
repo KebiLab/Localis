edbefd6470d8f769c991991e8770e79e37d111fe
KebiLab <kebi.alexandr@mail.ru>
2026-08-04 16:56:11 +0300

    style(desktop): align header and refine status type

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource-variable/jetbrains-mono";
 import "@fontsource-variable/manrope";
+import "@fontsource-variable/sora";
 
 import { App } from "./App";
 import "./styles.css";
